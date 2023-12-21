2e72e669e91c2768f24ccf2d30cc8f70c41afb14
Fr. Andrew Hedstrom <andrew.hedstrom@gmail.com>
2023-12-20 21:12:23 -0800

    testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
    site: 'https://catholicoffice.com',
-   output: 'hybrid',
-   adapter: node({
-     mode: 'standalone',
-   }),
+
    server: {
     host: '0.0.0.0'
   },
